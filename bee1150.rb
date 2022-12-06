23e6ec8a5d191532979d151866686fdce6997c99
Philipe Rodrigues <dev.philipe@outlook.com>
2022-12-06 15:56:12 -0300

    exercicio 1150 aceito

---
+++ bee1150.rb
@@ -0,0 +1,45 @@
+=begin
+            ULTRAPASSANDO Z
+
+    Faça um programa que leia dois inteiros: X e Z (devem ser lidos tantos valores para Z quantos necessários, 
+    até que seja digitado um valor maior do que X para ele). Conte quantos números inteiros devem ser somados em 
+        sequência (considerando o X nesta soma) para que a soma ultrapasse a Z o mínimo possível. Escreva o valor 
+        final da contagem.
+
+    A entrada pode conter, por exemplo, os valores 21 21 15 30. Neste caso, é então assumido o valor 21 para X 
+    enquanto os valores 21 e 15 devem ser desconsiderados pois são menores ou iguais a X. Como o valor 30 está 
+    dentro da especificação (maior do que X) ele será válido e então deve-se processar os cálculos para apresentar
+         na saída o valor 2, pois é a quantidade de valores somados para se produzir um valor maior do que 30 (21 + 22).
+
+    Entrada
+    A entrada contém somente valores inteiros, um por linha, podendo ser positivos ou negativos. O primeiro valor
+     da entrada será o valor de X. A próxima linha da entrada irá conter Z. Se Z não atender a especificação do
+         problema, ele deverá ser lido novamente, tantas vezes quantas forem necessárias.
+
+    Saída
+    Imprima uma linha com um número inteiro que representa a quantidade de números inteiros que devem ser somadas,
+     de acordo com a especificação acima.
+=end
+
+## criar um array que é a soma de x e inserir a cada loop um valor x+1 dentro do array e extrair essa soma.
+
+valores = gets.chomp.split(' ')
+x = valores[0].to_i
+z = valores[1].to_i
+
+until z > x 
+    z = gets.chomp.to_i
+end
+
+numeros = [x]
+soma = 0
+numeros.each { |i| soma += i}
+
+while soma < z do 
+    numeros.push(x + 1)
+    soma = 0
+    numeros.each { |i| soma += i}
+    x += 1
+end
+
+puts numeros.size
