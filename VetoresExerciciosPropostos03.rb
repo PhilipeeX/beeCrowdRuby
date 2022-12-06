0842e5e5fb4698a0a29721e7dc4ca618aeeab3bc
Philipe Rodrigues <dev.philipe@outlook.com>
2022-12-06 11:43:27 -0300

    VetoresExerciciosPropostos

--- VetoresExerciciosPropostos03.rb
+++ VetoresExerciciosPropostos03.rb
@@ -1,4 +1,31 @@
 =begin
     Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida, mostrar na tela o maior número
     do vetor (supor não haver empates). Mostrar também a posição do maior elemento.
+        Exemplo:
+    Entrada                         Saída
+    6
+    8.0 4.0 10.0 14.0 13.0 7.0
+                                    14.0
+                                    3
 =end
+
+vetor = Array.new(gets.chomp.to_i)
+vetor = gets.split(' ')
+i = 0
+vetor2 = []
+while i < vetor.size do
+    vetor2.push(vetor[i].to_f)
+    i += 1
+end
+i = 0
+maior = 0
+indice_do_maior = 0
+vetor2.each_with_index do |valor, indice|
+    if valor > maior
+        puts "funcionou"
+        maior = valor
+        indice_do_maior = indice
+    end
+end
+puts maior
+puts indice_do_maior
--- VetoresExerciciosPropostos06.rb
+++ VetoresExerciciosPropostos06.rb
@@ -1,4 +1,13 @@
 =begin
     Fazer um programa para ler um vetor de N números reais. Em seguida, mostrar na tela a média aritmética de todos
     elementos. Depois mostrar todos os elementos do vetor que estejam abaixo da média.
+
+        Exemplo:
+        Entrada                             Saída
+4
+10.0 15.5 13.2 9.8
+                                            12.125
+                                            10.0
+                                            9.8
 =end
+
