cc76beb333f8aa4f2a31a0814316f8800495268e
Philipe Rodrigues <dev.philipe@outlook.com>
2022-12-07 11:50:31 -0300

    exercicio 1160 refazer

--- bee1160.rb
+++ bee1160.rb
@@ -31,23 +31,8 @@
 =end
 
 casos_de_teste = gets.chomp.to_i
-i = 1
+i = 0
 
-while i <= casos_de_teste do
-    valores_x_y = gets.chomp.split(' ')
-    x = valores_x_y[0].to_i
-    y = valores_x_y[1].to_i
-
-    j = 1
-    impar = 0
-    while j <= y do
-        if x % 2 != 0
-            impar += x 
-            j += 1 
-        end
-        x += 1
-    end
-    puts "#{impar}"
-
-    i += 1 
+while i < casos_de_teste do
+     
 end
