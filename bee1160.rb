390419e610a2dc97b6e2d286a53340bdd6f12317
Philipe Rodrigues <dev.philipe@outlook.com>
2022-12-07 11:48:46 -0300

    exercicio 1160 aceito

--- bee1160.rb
+++ bee1160.rb
@@ -30,3 +30,24 @@
     contrário você poderá receber como resposta da submissão deste problema "Time Limit Exceeded".
 =end
 
+casos_de_teste = gets.chomp.to_i
+i = 1
+
+while i <= casos_de_teste do
+    valores_x_y = gets.chomp.split(' ')
+    x = valores_x_y[0].to_i
+    y = valores_x_y[1].to_i
+
+    j = 1
+    impar = 0
+    while j <= y do
+        if x % 2 != 0
+            impar += x 
+            j += 1 
+        end
+        x += 1
+    end
+    puts "#{impar}"
+
+    i += 1 
+end
